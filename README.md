aee55a9af90d771b0c6bf240246162092ed9196b
Kahlil Ello <ellokahlil@gmail.com>
2024-04-13 22:10:28 -0400

    Added Streamlit. Going to work on kinks and tracebacks later

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ##### Technical aspects
     - In order to pip install, will need to refer to the pip folder (/usr/bin/pip3). Ex: /usr/bin/pip3 install matplotlib
-
+    - Streamlit side will be based off of: 
+        https://github.com/aicglabs/meetups-and-webinars/blob/main/2022-06-27--Meetup-Snowflake-Streamlit/streamlit_meetup.ipynb
 ##### Fun Side
 - ML Will be a lof of machine learning checks on the commodities_12_22.csv 
 - Added plots.
--- Streamlit.py
+++ Streamlit.py
@@ -19,59 +19,72 @@ df = pd.read_csv(data_path)
 # Data Cleaning 
 commoditiesDf = df.dropna(axis=0)
 
-
 #Gold Table
 goldDf = commoditiesDf[['Date','Gold']]
-print(goldDf)
 
-# Visualize Gold Table w/ LoBF
-
-dates = goldDf['Date'].values
-gold_prices = goldDf['Gold'].values
+# Set up Streamlit App
+st.title('Gold Prices Visualization')
 
 
-# Assuming you have already imported your data and created `dates` and `gold_prices`
-plt.figure(figsize=(12,6))
 
-# Plot the scatter points for gold prices
-plt.plot(dates, gold_prices, label='Gold Prices')
+# Function isualize Gold Table w/ LoBF
 
-# Calculate the coefficients of the line of best fit (1st-degree polynomial)
-coefficients = np.polyfit(mdates.date2num(dates), gold_prices, 1)
+def visualize(goldDf):
+    dates = goldDf['Date'].values
+    gold_prices = goldDf['Gold'].values
 
-# Create the line of best fit equation
-line_of_best_fit = np.poly1d(coefficients)
 
-# Plot the line of best fit
-plt.plot(dates, line_of_best_fit(mdates.date2num(dates)), color='red', label='Line of Best Fit', linestyle='--')
+    # Assuming you have already imported your data and created `dates` and `gold_prices`
+    plt.figure(figsize=(12,6))
 
-plt.title("Gold Prices Over Time")
-plt.ylabel('Price')
-plt.xlabel('Date')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
-plt.show()
+    # Plot the scatter points for gold prices
+    plt.plot(dates, gold_prices, label='Gold Prices')
+
+    # Calculate the coefficients of the line of best fit (1st-degree polynomial)
+    coefficients = np.polyfit(mdates.date2num(dates), gold_prices, 1)
+
+    # Create the line of best fit equation
+    line_of_best_fit = np.poly1d(coefficients)
+
+    # Plot the line of best fit
+    plt.plot(dates, line_of_best_fit(mdates.date2num(dates)), color='red', label='Line of Best Fit', linestyle='--')
+
+    plt.title("Gold Prices Over Time")
+    plt.ylabel('Price')
+    plt.xlabel('Date')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+
+    st.pyplot(plt)
+
+# Visualize the Gold Prices
+visualize(goldDf)
 
 
 # Create the Slider
-max_date = dates.max()
-min_date = dates.min()
+max_date = pd.to_datetime(goldDf['Date']).max()
+min_date = pd.to_datetime(goldDf['Date']).min()
 
-with st.container():
-    values = st.slider(
+values = st.slider(
         "Gold Prices Date Range",
         min_value = min_date,
         max_value = max_date,
         value = (min_date, max_date)
     )
 
-gold_prices['Date'] = gold_prices.to_datetime(gold_prices["Date"]).dt.date
-gold_prices = gold_prices.loc[(gold_prices['Date'] >= values[0])] & (gold_prices['Date']<= values[1])
+# Filter Data based on selected date range
+filtered_goldDf = goldDf.loc[(goldDf['Date'] >= values[0]) & (goldDf['Date'] <= values[1])]
 
-# Creating column to distinguish month/year date
-gold_prices['MONTH'] = gold_prices.DatetimeIndex(gold_prices['Date']).month.map("{:02}".format).astype(str)
-gold_prices['YEAR'] = gold_prices.DatetimeIndex(gold_prices['Date']).year.map("{:02}".format).astype(str)
-gold_prices['MONTH_YEAR'] = gold_prices['YEAR'] + '-' + gold_prices['MONTH']
+# Display filtered data
+st.write(filtered_goldDf)
 
-# Creating aggregated Datasets for visualizations
+# Everything below is from the previous work, may not be needed
+
+# gold_prices['Date'] = gold_prices.to_datetime(gold_prices["Date"]).dt.date
+# gold_prices = gold_prices.loc[(gold_prices['Date'] >= values[0])] & (gold_prices['Date']<= values[1])
+
+# # Creating column to distinguish month/year date
+# gold_prices['MONTH'] = gold_prices.DatetimeIndex(gold_prices['Date']).month.map("{:02}".format).astype(str)
+# gold_prices['YEAR'] = gold_prices.DatetimeIndex(gold_prices['Date']).year.map("{:02}".format).astype(str)
+# gold_prices['MONTH_YEAR'] = gold_prices['YEAR'] + '-' + gold_prices['MONTH']
