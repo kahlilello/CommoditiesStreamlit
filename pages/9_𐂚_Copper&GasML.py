5a6f5d3f1569b6b29bc536e6d9df84197e0bce9b
Kahlil Ello <ellokahlil@gmail.com>
2024-05-07 18:30:28 -0400

    Fixed an error (Heatmap, cmap = 'TlGnBu' when it should be 'YlGnBu'

--- pages/9_𐂚_Copper&GasML.py
+++ pages/9_𐂚_Copper&GasML.py
@@ -93,7 +93,7 @@ def visualize(data, commodity, date_range):
     numeric_dates = mdates.date2num(dates_sorted)
     data = np.vstack((numeric_dates, prices_sorted)).T
     correlation_matrix = np.corrcoef(data, rowvar=False)
-    sns.heatmap(correlation_matrix, annot = True, cmap = "TlGnBu", ax = ax4, xticklabels=["Date", f"{commodity} Price"], yticklabels = ["Date", f"{commodity} Price"])
+    sns.heatmap(correlation_matrix, annot = True, cmap = "YlGnBu", ax = ax4, xticklabels=["Date", f"{commodity} Price"], yticklabels = ["Date", f"{commodity} Price"])
     ax4.set_title(f"{commodity} Price Correlation Heatmap")
 
     plt.tight_layout()
