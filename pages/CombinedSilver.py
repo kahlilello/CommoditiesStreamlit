b6bdf9213487a6492dfe1468e3c0fd70fa7c796b
Kahlil Ello <ellokahlil@gmail.com>
2024-05-30 19:21:35 -0400

    Ax1.set_title

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -71,3 +71,4 @@ def visualize(goldDf, date_range):
     ax1 = axes[0]
     ax1.plot(dates_sorted, gold_prices_sorted, label='Gold Prices')
     ax1.plot(dates, predicted_prices, color='orange', label='MLP (Model)')
+    ax1.set_title("Gold Prices Over Time")
