870c01be76d3b3be526a821c7e8ce8ab26225e6f
Kahlil Ello <ellokahlil@gmail.com>
2024-06-11 12:57:51 -0500

    Added date range

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -97,4 +97,4 @@ st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silv
 
 # Visualize the Gold Prices
 
-fig2 = MLvisualize(goldDf)
+fig2 = MLvisualize(goldDf, date_range)
