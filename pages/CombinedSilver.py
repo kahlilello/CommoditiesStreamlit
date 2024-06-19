97904b1fd307d9f5cef3adcfaadac358ceed94f8
Kahlil Ello <ellokahlil@gmail.com>
2024-06-18 22:14:56 -0500

    Added scaler

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -126,7 +126,7 @@ def MLvisualize(silverDf, date_range):
     # Feature scaling
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
+    X_test_scaled = scaler.
 
 st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silver </p>", unsafe_allow_html=True)
 
