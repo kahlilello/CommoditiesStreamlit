823dcc3720ef79e7364a7c261c9880259b14d696
Kahlil Ello <ellokahlil@gmail.com>
2024-06-23 20:51:58 -0500

    fitting X_train_scaled

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -130,7 +130,7 @@ def MLvisualize(silverDf, date_range):
 
     # MLP Regressor model
     mlp_model = MLPRegressor(hidden_layer_sizes = (100,100), activation = 'relu', solver = 'adam', random_state = 42)
-    mlp_model.fit()
+    mlp_model.fit(X_train_scaled)
 
 st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silver </p>", unsafe_allow_html=True)
 
