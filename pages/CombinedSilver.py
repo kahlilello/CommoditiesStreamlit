4388f11aeb7b1ea79c5ef972a1231b582fb003a9
Kahlil Ello <ellokahlil@gmail.com>
2024-06-30 13:12:28 -0700

    s

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -146,6 +146,7 @@ def MLvisualize(silverDf, date_range):
     ax1 = axes[0]
     ax1.plot(dates_sorted, silver_prices_sorted, label = 'Silver Prices')
     ax1.plot(dates, predicted_prices, color = 'orange', label = 'MLP (Model)')
+    
 
 st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silver </p>", unsafe_allow_html=True)
 
