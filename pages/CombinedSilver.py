3bd0f54dd4376d830b67f9d5b0539e8f02bb21e2
Kahlil Ello <ellokahlil@gmail.com>
2024-07-03 13:58:28 -0500

    mse_test

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -154,6 +154,7 @@ def MLvisualize(silverDf, date_range):
 
     # Calculate and display MSE
     mse_train = mean_squared_error(y_train, predicted_prices_train)
+    mse_test = 
 
 st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silver </p>", unsafe_allow_html=True)
 
