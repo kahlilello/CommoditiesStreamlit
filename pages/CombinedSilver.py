9d9851f546fb345ea84653b227d92e74725724e1
Kahlil Ello <113486826+kahlilello@users.noreply.github.com>
2024-07-27 15:18:27 -0500

    Update CombinedSilver.py

--- pages/CombinedSilver.py
+++ pages/CombinedSilver.py
@@ -162,6 +162,8 @@ def MLvisualize(silverDf, date_range):
     ax2 = axes[1]
     sns.histplot(silver_prices_sorted, kde = True, color = "skyblue", ax = ax2)
     
+	#Test
+
 
 
 st.markdown(f"<p style='font-size:24px;font-weight:bold;'> Machine Learning Silver </p>", unsafe_allow_html=True)
