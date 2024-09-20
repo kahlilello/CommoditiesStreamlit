34603bbba97a2f61bea8782ac343274b214b3fc4
Kahlil Ello <ellokahlil@gmail.com>
2024-09-19 21:46:29 -0500

    Test

--- 1_📈_Main.py
+++ 1_📈_Main.py
@@ -6,6 +6,8 @@ import numpy as np
 from datetime import datetime
 import streamlit as st
 
+test
+
 # Data Ingestion
 data_path = "commodities_12_22.csv"
 # Read the CSV file into a pandas dataframe
